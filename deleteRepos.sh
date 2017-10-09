c824f9479134cf2b1033ab7c46b364f640e4d1d8
Fabian Kalweit <fabiankalweit@yahoo.de>
2017-10-09 15:19:29 +0200

    add delete Script
    script deletes repos in a organization
    repo-names <classname_user>

---
+++ deleteRepos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+token=$(<token.txt)
+
+echo "Your Token: $token"
+
+echo "class name:"
+read className
+
+echo "Path to names list (default ./names.txt):"
+read pathNames
+
+if [[ "$pathNames" = "" ]]; then
+  pathNames='names.txt'
+fi
+
+
+echo "name of the organization?"
+read orgName;
+
+
+for rn in $(<$pathNames); do
+  echo "the next name is $rn"
+  repoName="$className"'_'"$rn"
+  url="https://api.github.com/repos/$orgName/$repoName"
+  echo $url
+  curl -i -X DELETE -H "Authorization: token $token" $url
+done
